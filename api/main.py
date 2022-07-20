7577a9e8f57986f9738164124c3a36b9bd21b7ac
lucaseo <lucaseo0147@gmail.com>
2022-07-20 16:01:12 +0900

    init api dev.

--- api/main.py
+++ api/main.py
@@ -1,1 +1,21 @@
 # Put the code for your API here.
+
+from fastapi import FastAPI
+# from fastapi.middleware.cors import CORSMiddleware
+
+from .routers.hello_world import router as router_hello_world
+
+def get_application():
+    app = FastAPI(title="census_inference_api",
+                  version="v0.1",
+                  description="Inference API for census dataset (Udacity Project)")
+    app.include_router(router_hello_world, prefix='?', tags=['?'])
+    # app.add_middleware(
+    #     CORSMiddleware,
+    #     allow_credentials=False,
+    #     allow_methods=["*"],
+    #     allow_headers=["*"],
+    # )
+    return app
+
+app = get_application()
---
+++ api/routers/hello_world.py
@@ -0,0 +1,12 @@
+from fastapi.routing import APIRouter
+from fastapi.responses import JSONResponse
+
+router = APIRouter()
+
+@router.get("/")
+async def say_hello_world():
+    return JSONResponse(
+        content={
+            "msg": "IA 2.0 ML Inference API"
+        }
+    )
